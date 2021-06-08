1e43098ecf36c41ad00a2f053a44495450d455e8
MTAscorpion <55039208+MTAscorpion@users.noreply.github.com>
2021-06-08 15:08:25 +0700

    abc

---
+++ DemoEnglish/DemoEnglish/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DemoEnglish
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.dtgv_Data = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(126, 19);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(435, 26);
+            this.txt_name.TabIndex = 1;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.Location = new System.Drawing.Point(595, 14);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(88, 36);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // dtgv_Data
+            // 
+            this.dtgv_Data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv_Data.Location = new System.Drawing.Point(21, 65);
+            this.dtgv_Data.Name = "dtgv_Data";
+            this.dtgv_Data.RowTemplate.Height = 24;
+            this.dtgv_Data.Size = new System.Drawing.Size(758, 363);
+            this.dtgv_Data.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtgv_Data);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "TestAPI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.DataGridView dtgv_Data;
+    }
+}
+
---
+++ DemoEnglish/DemoEnglish/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace DemoEnglish
+{
+    public partial class MainForm : Form
+    {
+        public class NGUOIDUNG
+        {
+           
+            public string MA_NGUOIDUNG { get; set; }
+            public string MATKHAU { get; set; }
+            public string TEN_NGUOIDUNG { get; set; }
+            public string NGAYSINH { get; set; }
+            public Nullable<double> TONGDIEM { get; set; }
+            public string SDT { get; set; }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            this.PopulateDataGridView();
+        }
+
+        private void PopulateDataGridView()
+        {
+            string apiUrl = "http://localhost:1063/api/english";
+            object input = new
+            {
+                Name = txt_name.Text.Trim(),
+            };
+            string inputJson = (new JavaScriptSerializer()).Serialize(input);
+            WebClient client = new WebClient();
+            client.Headers["Content-type"] = "application/json";
+            client.Encoding = Encoding.UTF8;
+            string address = apiUrl + "/GetNGUOIDUNG";
+            string json = client.UploadString(address, inputJson);
+
+            dtgv_Data.DataSource = (new JavaScriptSerializer()).Deserialize<List<NGUOIDUNG>>(json);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //this.PopulateDataGridView();
+        }
+
+    }
+}
